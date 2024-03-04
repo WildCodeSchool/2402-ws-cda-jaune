0afe17e2d7461bc6fea8e17906a5723e01c54f87
LoicBrassart <loic.brassart@gmail.com>
2024-03-04 15:11:52 +0100

    Add ads' data in an object and map on it

--- 240227-the-good-corner/frontend_boilerplate-main/src/pages/RecentAds.tsx
+++ 240227-the-good-corner/frontend_boilerplate-main/src/pages/RecentAds.tsx
@@ -1,17 +1,51 @@
 import AdCard from "./AdCard";
 
 export default function RecentAds() {
+  const ads = [
+    {
+      title:"/ads/table",
+      link:"Table",
+      price:120,
+      imgUrl:"/images/table.webp"
+    },
+    {
+      title:"Dame-jeanne",
+      link:"/ads/dame-jeanne",
+      price:75,
+      imgUrl:"/images/dame-jeanne.webp"
+    },
+    {
+      title:"Vide-poche",
+      link:"/ads/vide-poche",
+      price:4,
+      imgUrl:"/images/vide-poche.webp"
+    },
+    {
+      title:"Vaisselier",
+      link:"/ads/vaisselier",
+      price:900,
+      imgUrl:"/images/vaisselier.webp"
+    },
+    {
+      title:"Bougie",
+      link:"/ads/bougie",
+      price:8,
+      imgUrl:"/images/bougie.webp"
+    },
+    {
+      title:"porte-magazine",
+      link:"/ads/porte-magazine",
+      price:45,
+      imgUrl:"/images/porte-magazine.webp"
+    },
+  ]
     return (
 <main className="main-content">
       <h2>Annonces récentes</h2>
       <section className="recent-ads">
-      <AdCard imgUrl="/images/table.webp" link="/ads/table" price={120} title="Table" />
-      <AdCard imgUrl="/images/dame-jeanne.webp" link="/ads/dame-jeanne" price={75} title="Dame-jeanne" />
-      <AdCard imgUrl="/images/vide-poche.webp" link="/ads/vide-poche" price={4} title="Vide-poche" />
-      <AdCard imgUrl="/images/vaisselier.webp" link="/ads/vaisselier" price={900} title="Vaisselier" />
-      <AdCard imgUrl="/images/bougie.webp" link="/ads/bougie" price={8} title="Bougie" />
-      <AdCard imgUrl="/images/porte-magazine.webp" link="/ads/porte-magazine" price={45} title="porte-magazine" />
-
+        {ads.map(ad=>
+        <AdCard imgUrl={ad.imgUrl} link={ad.link} price={ad.price} title={ad.title} key={ad.title} />
+        )}
       </section>
     </main>
     )}
