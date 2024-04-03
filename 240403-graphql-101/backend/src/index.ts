e79fa4147843f3a561dc1d06df27e3459a5d49a5
LoicBrassart <loic.brassart@gmail.com>
2024-04-03 09:58:29 +0200

    Begin the setup for Apollo server [broken]

--- 240403-graphql-101/backend/src/index.ts
+++ 240403-graphql-101/backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { buildSchema } from "type-graphql";
+import { RecipeResolver } from "./resolvers/RecipeResolver";
 
 const port = 5000;
 const app = express();
@@ -6,6 +8,14 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+const startApollo = async () => {
+  const schema = await buildSchema({
+    resolvers: [RecipeResolver],
+  });
+  const apolloServer = new ApolloServer();
+};
+startApollo();
 
 app.listen(port, async () => {
   console.log(`project's backend listening on http://localhost:${port}`);
