3ecbc8af881a089ab13f9240104d9a7c80986e8f
LoicBrassart <loic.brassart@gmail.com>
2024-04-03 16:59:04 +0200

    Rollback to SQLite database + Add misc npm commands

--- 240227-the-good-corner/backend/src/data/config.ts
+++ 240227-the-good-corner/backend/src/data/config.ts
@@ -1,17 +1,11 @@
 import { DataSource } from "typeorm";
-import { Ad } from "./entities/ad";
-import { Category } from "./entities/category";
-import { Tag } from "./entities/tag";
 require("dotenv").config();
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DBNAME } = process.env;
 export const dataSource = new DataSource({
-  type: "mysql",
-  host: DB_HOST,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DBNAME,
-
-  entities: [Ad, Category, Tag],
-  synchronize: true,
+  type: "sqlite",
+  database: "./data/db.sqlite",
+  entities: ["src/data/entities/*.ts"],
+  synchronize: false,
+  migrations: ["data/*.ts"],
+  migrationsTableName: "migrations",
 });
