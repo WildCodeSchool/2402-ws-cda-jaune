6c4c73ec780c75d42678a546dcad46d9c7cd34b7
LoicBrassart <loic.brassart@gmail.com>
2024-04-29 10:40:38 +0200

    Add answers for A2

--- Algos/S03+/src/challenges/A2/answer.ts
+++ Algos/S03+/src/challenges/A2/answer.ts
@@ -9,15 +9,27 @@
  */
 
 // ↓ uncomment bellow lines and add your response!
-/*
 export default function ({
   categories,
 }: {
   categories: Category[];
 }): CategoryWithTags[] {
-  return [];
+  return categories.map((category) => {
+    // 1ere implementation: reduce, Set
+    const tags = category.ads.reduce((acc: Array<string>, ad) => {
+      return [...acc, ...ad.tags];
+    }, []);
+    const categoryWithTags: CategoryWithTags = {
+      ...category,
+      tags: [...new Set(tags)].sort(),
+    };
+    return categoryWithTags;
+
+    //2nde implementation (@Logan): Set, flatMap (nécessite es2019)
+    const tags = [...new Set(category.ads.flatMap((ad) => ad.tags))].sort();
+    return { ...category, tags };
+  });
 }
-*/
 
 // used interfaces, do not touch
 interface Ad {
